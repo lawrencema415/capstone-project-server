3c6fea34cfc299d191edf4dc74c2b690fc3bfca7
Lawrence <lawrencema415@gmail.com>
2019-09-03 15:20:08 -0700

    adding login/logout/verify

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,6 +4,7 @@ const bcrypt = require('bcryptjs');
 const validate = require('../validation/register');
 const db = require('../models');
 
+// Register route
 const register = (req,res) => {
   const {errors, invalid} = validate(req.body);
 
@@ -38,8 +39,52 @@ const register = (req,res) => {
   });
 };
 
+// Login route
+const login = (req,res) => {
+  if (!req.body.email || !req.body.password) {
+    return res.status(400).json({ status: 400, message:"Please enter email and password."});
+  }
+
+  db.User.findOne({email: req.body.email}, (err, foundUser) => {
+    if (err) return res.status(500).json({ status: 500, message:"Something went wrong, please try again."});
+
+    if(!foundUser) {
+      return res.status(400).json({ status: 400, message: "Email or password is incorrect."});
+    }
+
+    bcrypt.compare(req.body.password, foundUser.password, (err, isMatch) => {
+      if (err) return res.status(500).json({ status: 500, message: "Something went wrong, please try again."});
+
+      if (isMatch) {
+        req.session.loggedIn = true;
+        req.session.currentUser = { id: foundUser._id };
+        return res.status(200).json({ status: 200, message :"Success", id: foundUser._id});
+      } else {
+        return res.status(400).json({ status: 400, message: "Email or password is incorrect"});
+      };
+    });
+  });
+};
+
+// Logout route
+const logout = (req,res) => {
+  req.session.destroy(err => {
+    if (err) return res.json({status:400, message: "Something went wrong, please try again."});
+    res.sendStatus(200);
+  });
+};
+
+// Verify route
+const verify = (req,res) => {
+  if (!req.session.currentUser) return res.status(401).json({ status: 401, message: "unauthorized"});
+  res.status(200).json({status: 200, message: `Current user verified. User ID is ${req.session.currentUser.id}`})
+};
+
 
 
 module.exports = {
-  register
-}
+  register,
+  login,
+  logout,
+  verify
+};
--- routes/auth.js
+++ routes/auth.js
@@ -3,5 +3,8 @@ const router = express.Router();
 const ctrl = require('../controllers');
 
 router.post('/register', ctrl.auth.register);
+router.post('/login', ctrl.auth.login);
+router.post('/verify', ctrl.auth.verify);
+router.post('/logout',ctrl.auth.verify);
 
 module.exports = router;
