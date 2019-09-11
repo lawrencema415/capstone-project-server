cdc77061ab738ec504db2c955b923118230ddccb
Lawrence <lawrencema415@gmail.com>
2019-09-11 09:09:01 -0700

    uncommenting seed

--- seed.js
+++ seed.js
@@ -2,10 +2,10 @@ const songs = require('./musicSeed.json');
 const db = require('./models');
 const artists = require('./artistSeed.json');
 
-// db.Song.create(songs, (err,createdSong) => {
-//   if(err) return console.log(err);
-//   console.log(createdSong);
-// });
+db.Song.create(songs, (err,createdSong) => {
+  if(err) return console.log(err);
+  console.log(createdSong);
+});
 
 db.Artist.create(artists, (err,createdArtists) => {
   if(err) return console.log(err);
